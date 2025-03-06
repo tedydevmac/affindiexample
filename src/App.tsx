79116eaec49bbce2b194ac0e133a26c69c87c385
Goh Min Wen, Ted <ilovefirebase4@gmail.com>
2025-03-06 21:01:26 +0800

    temp

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { IoMdSettings } from "react-icons/io";
+import { FaCartShopping } from "react-icons/fa6";
+import { IoPersonCircleSharp } from "react-icons/io5";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="tabrow">
+        <img
+          src="../src/assets/icon.png"
+          alt="logo not found"
+          className="shoplogo"
+        />
+        <div className="icondiv">
+          <a href="">
+            <FaCartShopping className="icon" />
+          </a>
+          <a>
+            <IoPersonCircleSharp className="icon" />
+          </a>
+          <a href="">
+            <IoMdSettings className="icon" />
+          </a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
